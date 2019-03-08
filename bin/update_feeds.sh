15a0f030be887e43bd39c92754a642d1bcac1fac
François Michonneau <francois.michonneau@gmail.com>
2019-03-08 11:25:13 +0100

    rename file + get instructor data

--- bin/update_feeds.sh
+++ bin/update_feeds.sh
@@ -9,7 +9,10 @@ cd .. &&
     git clone https://github.com/carpentries/amy-feeds.git amy-feeds &&
     cd amy-feeds &&
     make workshops &&
-    find _data -name '*_plain.json' -exec cp {} ../"$REPO_NAME"/_data/ \;
+    find _data -name '*_plain.json' -exec cp {} ../"$REPO_NAME"/_data/ &&
+    make instructors &&
+    cp _data/amy_airports.json ../"$REPO_NAME"/_data/ &&
+    cp _data/publish_instructors.json ../"$REPO_NAME"/_data
 
 cd ../"$REPO_NAME"  || exit
 
@@ -17,5 +20,5 @@ git remote add deploy https://"$GITHUB_PAT_USER":"$GITHUB_PAT"@github.com/"$REPO
 
 git checkout gh-pages
 git add _data/*_plain.json
-git commit -m "[ci skip] update workshop data"
+git commit -m "[ci skip] update workshop and instructor data"
 git push deploy gh-pages
