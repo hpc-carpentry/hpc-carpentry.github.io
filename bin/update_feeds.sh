c4a875500c9a6ddaf57085873bca85a8277d6478
François Michonneau <francois.michonneau@gmail.com>
2019-03-08 16:39:44 +0100

    use local geojson file

--- bin/update_feeds.sh
+++ bin/update_feeds.sh
@@ -13,14 +13,15 @@ cd .. &&
     curl --remote-name-all https://feeds.carpentries.org/all_workshops_plain.json &&
     curl --remote-name-all https://feeds.carpentries.org/all_instructors.json &&
     curl --remote-name-all https://feeds.carpentries.org/publish_instructors.json &&
-    find . -name '*.json' -exec cp {} ../"$REPO_NAME"/_data/ \;
+    find . -name '*.json' -exec cp {} ../"$REPO_NAME"/_data/ \; &&
+    cp _data/all_instructors.json files/geojson/all_instructors.json
 
 cd ../"$REPO_NAME"  || exit
 
 git remote add deploy https://"$GITHUB_PAT_USER":"$GITHUB_PAT"@github.com/"$REPO_ORG"/"$REPO_NAME".git
 
 git checkout gh-pages
-git add _data/*.json
+git add _data/*.json files/geojson/all_instructors.json
 git commit -m "[ci skip] update workshop and instructor data"
 git push deploy gh-pages
 
