7d71715478c5821b6f32584b68778451a479bf9a
François Michonneau <francois.michonneau@gmail.com>
2019-03-08 12:03:55 +0100

    get data from feed instead of building locally

--- bin/update_feeds.sh
+++ bin/update_feeds.sh
@@ -4,23 +4,24 @@ REPO_ORG=carpentries
 REPO_NAME=carpentries.org
 GITHUB_PAT_USER=fmichonneau # user who generated the GITHUB PAT used here
 
+
+
 cd .. &&
-    rm -rf amy-feeds/ &&
-    git clone https://github.com/carpentries/amy-feeds.git amy-feeds &&
-    cd amy-feeds &&
-    make workshops &&
-    find _data -name '*_plain.json' -exec cp {} ../"$REPO_NAME"/_data/ \; &&
-    make instructors &&
-    cp _data/amy_airports.json ../"$REPO_NAME"/_data/ &&
-    cp _data/publish_instructors.json ../"$REPO_NAME"/_data
+    mkdir feed-data &&
+    cd feed-data &&
+    curl --remote-name-all http://feeds.carpentries.org/{all,DC,LC,SWC,TTT}_{past,upcoming}_workshops_plain.json &&
+    curl --remote-name-all http://feeds.carpentries.org/all_workshops_plain.json &&
+    curl --remote-name-all http://feeds.carpentries.org/amy_airports.json &&
+    curl --remote-name-all http://feeds.carpentries.org/publish_instructors.json &&
+    find . -name '*.json' -exec cp {} ../"$REPO_NAME"/_data/ \;
 
 cd ../"$REPO_NAME"  || exit
 
 git remote add deploy https://"$GITHUB_PAT_USER":"$GITHUB_PAT"@github.com/"$REPO_ORG"/"$REPO_NAME".git
 
 git checkout gh-pages
-git add _data/*_plain.json
-git add _data/amy_airports.json
-git add _data/publish_instrutors.json
+git add _data/*.json
 git commit -m "[ci skip] update workshop and instructor data"
 git push deploy gh-pages
+
+rm -rf ../feed-data
