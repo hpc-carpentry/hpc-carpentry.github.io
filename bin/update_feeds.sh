2569b36f399fc6727045fc556184ae327ffb4360
François Michonneau <francois.michonneau@gmail.com>
2020-05-20 13:40:51 +0200

    test pdf generation (#772)

--- bin/update_feeds.sh
+++ bin/update_feeds.sh
@@ -6,6 +6,8 @@ GITHUB_PAT_USER=fmichonneau # user who generated the GITHUB PAT used here
 
 mkdir -p ../"$REPO_NAME"/files/geojson/
 
+## data feeds
+
 cd .. &&
     mkdir feed-data &&
     cd feed-data &&
@@ -23,10 +25,25 @@ cd .. &&
 
 cd ../"$REPO_NAME"  || exit
 
+## PDF files
+
+MD_DIR=pages
+MD_FILE=online-workshop-recommendation.md
+PDF_FILE=${MD_FILE%.md}.pdf
+PDF_OUT_DIR=../"$REPO_NAME"/files/pdf
+
+mkdir -p PDF_OUT_DIR
+
+cd .. &&
+    pandoc --output "$PDF_OUT_DIR"/"$PDF_FILE" "$MD_DIR"/"$MD_FILE" --pdf-engine=xelatex
+
+## Git
+
 git remote add deploy https://"$GITHUB_PAT_USER":"$GITHUB_PAT"@github.com/"$REPO_ORG"/"$REPO_NAME".git
 
 git checkout gh-pages
 git add _data/*.json files/geojson/all_instructors_by_airport.geojson
+git add files/pdf/*.pdf
 git commit -m "[ci skip] update feed data"
 git push deploy gh-pages
 
